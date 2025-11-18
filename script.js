533ff96ecb535b78ecac4b84ae82158268265aaa
Zhanna <kezhanna.03@mail.ru>
2025-11-19 01:04:39 +0500

    tasks 9-10

--- script.js
+++ script.js
@@ -22,3 +22,10 @@ closeTaskFormBtn.addEventListener("click", () => {
 });
 
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e)=>{e.preventDefault();});
