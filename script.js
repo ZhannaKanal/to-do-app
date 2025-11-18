12b0ed34a8ab666a26becca621b77fc69fcbbd7b
Zhanna <kezhanna.03@mail.ru>
2025-11-19 00:39:00 +0500

    task 6

--- script.js
+++ script.js
@@ -13,3 +13,6 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
