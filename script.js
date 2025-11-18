49ba1cce60bb48f1d5ba3e6f8c6b27bac21a9388
Zhanna <kezhanna.03@mail.ru>
2025-11-19 04:03:41 +0500

    tasks 40-49

--- script.js
+++ script.js
@@ -25,13 +25,15 @@ const addOrUpdateTask = () => {
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
-  
+
+
   updateTaskContainer()
   reset()
 };
 
 const updateTaskContainer = () => {
-tasksContainer.innerHTML = "";
+  tasksContainer.innerHTML = "";
+
   taskData.forEach(
     ({ id, title, date, description }) => {
         tasksContainer.innerHTML += `
@@ -39,13 +41,39 @@ tasksContainer.innerHTML = "";
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button> 
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `
     }
   );
 };
+
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+}
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");  
+}
 
 const reset = () => {
   titleInput.value = "";
